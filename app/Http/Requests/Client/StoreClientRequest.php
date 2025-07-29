963760df3b43065c3dfe3c23b9ee853abcd4b3fd
UsmanGhani552 <usman.centosquare@gmail.com>
2025-07-29 17:34:04 +0500

    add address

--- app/Http/Requests/Client/StoreClientRequest.php
+++ app/Http/Requests/Client/StoreClientRequest.php
@@ -25,7 +25,8 @@ class StoreClientRequest extends FormRequest
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255|unique:users,email',
             'username' => 'string|max:255|unique:users,username',
-            'phone' => 'required|string|max:20',
+            'phone' => 'required|string|max:12',
+            'address' => 'string',
             'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'password' => 'required|string|min:8',
         ];
