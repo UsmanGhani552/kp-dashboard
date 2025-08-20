5744a2940c797e8f8b1c17ad78df848049ccec4f
UsmanGhani552 <usman.centosquare@gmail.com>
2025-08-20 17:57:20 +0500

    changes

--- app/Mail/OrderCreated.php
+++ app/Mail/OrderCreated.php
@@ -40,7 +40,7 @@ class OrderCreated extends Mailable
 
         return $this
             ->subject("NEW ORDER #{$this->invoice->id} - {$brandName}")
-            ->from($brandName)
+            ->from($brand->email,$brandName)
             ->markdown('emails.orders.create', [
                 'invoice' => $this->invoice,
                 'brandLogo' => $brandLogo,
